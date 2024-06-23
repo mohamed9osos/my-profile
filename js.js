ffe22a1a34925bb3416bbb3246829c17f23e9f80
medo osos <121305517+mohamed9osos@users.noreply.github.com>
2024-06-23 20:15:47 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,35 @@
+let img_one = document.querySelector('.img_one')
+let img_two = document.querySelector('.img_two')
+let img_three = document.querySelector('.img_three')
+let img_four = document.querySelector('.img_four')
+let img_five = document.querySelector('.img_five')
+let img_six = document.querySelector('.img_six')
+let btn = document.querySelector('.btn')
+
+
+
+img_one.addEventListener('click', () => {
+    window.open('https://mohamed9osos.github.io/pro3/', "_blank", "")
+})
+img_two.addEventListener('click', () => {
+    window.open('https://mohamed9osos.github.io/pro6/', "_blank", "")
+})
+img_three.addEventListener('click', () => {
+    window.open('https://mohamed9osos.github.io/pro5/', "_blank", "")
+})
+img_four.addEventListener('click', () => {
+    window.open('https://mohamed9osos.github.io/pro4/', "_blank", "")
+})
+img_five.addEventListener('click', () => {
+    window.open('https://mohamed9osos.github.io/pro1/', "_blank", "")
+})
+img_six.addEventListener('click', () => {
+    window.open('https://mohamed9osos.github.io/pro2/', "_blank", "")
+})
+btn.addEventListener('click', () => {
+    window.open('https://wa.me/+905365486607', "_blank", "")
+})
+
+
+
+// https://wa.me/+905365486607
